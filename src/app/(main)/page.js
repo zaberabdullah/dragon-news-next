43aa3f29d984cce22f7e7d34c77a063a82e8d478
zaberabdullah <zaberabdullah120@gmail.com>
2026-04-26 00:05:43 +0600

    fetch data and show all categories

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,5 +1,32 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <h2 className="text-2xl font-bold text-black">Home Page</h2>;
+async function getCategories() {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+  return data.data;
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+  return (
+    <div className="container mx-auto grid grid-cols-12 gap-4 p-4 my-[50px]">
+      <div className=" col-span-3">
+        <h2 className="text-lg font-bold mb-4">All categories</h2>
+        <ul className="flex flex-col gap-1 mt-6">
+          {categories.news_category.map((category) => {
+            return (
+              <li
+                key={category.category_id}
+                className="bg-slate-200 hover:bg-slate-300 p-2 rounded-md text-md text-center font-bold"
+              >
+                {category.category_name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="text-3xl font-bold bg-purple-100 col-span-6">All news</div>
+      <div className="text-3xl font-bold bg-yellow-100 col-span-3">Social icons</div>
+    </div>
+  );
 }
