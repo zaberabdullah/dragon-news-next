8d26857b7b174e83feef8aceaea00b0870b5589a
zaberabdullah <zaberabdullah120@gmail.com>
2026-04-27 19:46:00 +0600

    connected mongo uri

---
+++ src/lib/auth.js
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient("process.env.MONGO_URI");
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
