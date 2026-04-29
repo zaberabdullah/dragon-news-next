0b6825d08220c02fc2ee255f5556376590924a1a
zaberabdullah <zaberabdullah120@gmail.com>
2026-04-29 16:32:25 +0600

    login functionality updated

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -24,6 +24,14 @@ const LoginPage = () => {
       callbackURL: "/",
     });
     console.log(res, error);
+
+     if (error) {
+      alert(error.message);
+    }
+    if (res) {
+      alert("Login successful");
+    }
+
   };
 
   return (
